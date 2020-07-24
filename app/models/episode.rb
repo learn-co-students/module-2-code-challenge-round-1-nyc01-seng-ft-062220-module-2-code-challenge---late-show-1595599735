341a16bbd9f1cdb80f8e0d85a2139a5f8c95965a
sonyavera <sonyavgould@gmail.com>
2020-07-24 11:54:35 -0400

    advanced deliverable

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -2,4 +2,8 @@ class Episode < ApplicationRecord
     has_many :appearances
     has_many :guests, through: :appearances
 
+    def episode_appearance_rating(guest)
+        self.appearances.find_by_id(guest_id).rating
+    end
+
 end
