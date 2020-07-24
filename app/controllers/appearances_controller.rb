5e05a2f51ae060db4724c9868327770c0ba5c74b
kylejb <47502769+kylejb@users.noreply.github.com>
2020-07-24 11:21:41 -0400

    Final MVP Deliverable

--- app/controllers/appearances_controller.rb
+++ app/controllers/appearances_controller.rb
@@ -7,8 +7,13 @@ class AppearancesController < ApplicationController
     end
 
     def create
-       @appearance = Appearance.create(appearance_params)
-        redirect_to episode_path(@appearance.episode.id)
+
+        @appearance = Appearance.create(appearance_params)
+        if @appearance.valid?
+            redirect_to episode_path(@appearance.episode.id)
+        else
+            redirect_to new_appearance_path, form_errors: @appearance.errors.full_messages
+        end
     end
 
     private
