229750ae6c7946940622eafbade75fbb1ceb6c84
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2020-07-24 11:56:02 -0400

    matt's mod 2 code challenge

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -3,4 +3,14 @@ class GuestsController < ApplicationController
   def index
     @guests = Guest.all
   end
+
+  def show
+    @guest = Guest.find(params[:id])
+    @my_appearances = @guest.appearances
+    @my_episodes = Guest.all
+  end
+
+#**NOTE: I know that the last link on guest show page was just supposed to only link from the date (not the full string). Would just have the first argument of the link_to be the date instead of the full line.
+#I think know how to do it (by splitting the strings and concatenating so I could just link to the second string) but was running low on time and didn't want to break it right before we had to submit. Hope that's ok.
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Appearance.destroy_all
 Guest.destroy_all
+Episode.destroy_all
+
 
 require 'csv'
 
@@ -25,3 +28,26 @@ date = Date.parse('2015-09-08')
   date = date.next
 end
 
+100.times do
+  Appearance.create(rating: rand(1..5), guest: Guest.all.sample, episode: Episode.all.sample)
+end
+# a1 = Appearance.create(rating: 5, guest: Guest.all.sample, episode: Episode.all.sample)
+# a2 = Appearance.create(rating: 4, guest: Guest.all.sample, episode: Episode.all.sample)
+# a3 = Appearance.create(rating: 3, guest: Guest.all.sample, episode: Episode.all.sample)
+# a4 = Appearance.create(rating: 2, guest: Guest.all.sample, episode: Episode.all.sample)
+# a5 = Appearance.create(rating: 1, guest: Guest.all.sample, episode: Episode.all.sample)
+# a6 = Appearance.create(rating: 5, guest: Guest.all.sample, episode: Episode.all.sample)
+# a7 = Appearance.create(rating: 4, guest: Guest.all.sample, episode: Episode.all.sample)
+# a8 = Appearance.create(rating: 3, guest: Guest.all.sample, episode: Episode.all.sample)
+# a9 = Appearance.create(rating: 2, guest: Guest.all.sample, episode: Episode.all.sample)
+# a10 = Appearance.create(rating: 1, guest: Guest.all.sample, episode: Episode.all.sample)
+# a11 = Appearance.create(rating: 1, guest: Guest.all.sample, episode: Episode.all.sample)
+# a12 = Appearance.create(rating: 5, guest: Guest.all.sample, episode: Episode.all.sample)
+# a13 = Appearance.create(rating: 1, guest: Guest.all.sample, episode: Episode.all.sample)
+# a14 = Appearance.create(rating: 3, guest: Guest.all.sample, episode: Episode.all.sample)
+# a15 = Appearance.create(rating: 1, guest: Guest.all.sample, episode: Episode.all.sample)
+# a16 = Appearance.create(rating: 2, guest: Guest.all.sample, episode: Episode.all.sample)
+# a17 = Appearance.create(rating: 1, guest: Guest.all.sample, episode: Episode.all.sample)
+# a18 = Appearance.create(rating: 5, guest: Guest.all.sample, episode: Episode.all.sample)
+# a19 = Appearance.create(rating: 2, guest: Guest.all.sample, episode: Episode.all.sample)
+# a20 = Appearance.create(rating: 1, guest: Guest.all.sample, episode: Episode.all.sample)
