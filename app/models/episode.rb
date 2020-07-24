9eb0979fd60da30c67437c442f8b2ad199a25598
dukhyunko93 <dukhyunko93@gmail.com>
2020-07-24 11:56:15 -0400

    final

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -6,4 +6,8 @@ class Episode < ApplicationRecord
         self.appearances.map { |a| a.rating}.sum / self.appearances.count
     end
 
+    def top_rating
+        self.appearances.sort_by {|t| t.rating}
+    end
+
 end
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,9 +1,5 @@
 class Guest < ApplicationRecord
     has_many :appearances
     has_many :episodes, through: :appearances
-
-    def top_rate
-        self.appearances.sort_by {|t| t.rating}
-    end
     
 end
