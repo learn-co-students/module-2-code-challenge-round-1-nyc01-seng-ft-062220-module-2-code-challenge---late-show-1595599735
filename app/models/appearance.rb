e6f9e593a388e487667d196fabf74253480a74b8
jakemeout <jacobosity@gmail.com>
2020-07-24 11:54:28 -0400

    tried advanced

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -3,6 +3,9 @@ class Appearance < ApplicationRecord
     belongs_to :guest
 
     validates_inclusion_of :rating, in: 1..5
-    
+    validates_uniqueness_of :episode_id, scope: [:guest_id]  # I think this would validate unique guests per episode no?
+
 end
+
+
 
