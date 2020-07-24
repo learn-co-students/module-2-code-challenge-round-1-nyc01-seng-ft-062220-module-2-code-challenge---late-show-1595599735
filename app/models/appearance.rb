7712cc7b0427f2cf892615b629f31799e0308df7
Luis Reyes <luisreyesxv@yahoo.com>
2020-07-24 11:43:20 -0400

    deliverables

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -3,5 +3,6 @@ class Appearance < ApplicationRecord
     belongs_to :guest
     
     validates :ratings, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 5, only_integer: true}
+    validates_uniqueness_of :guest_id, scope: :episode_id
 
 end
