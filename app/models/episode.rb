1511d2c1767df62b7ffb92af5899fd42a804000c
elandau91 <eli.b.landau@gmail.com>
2020-07-24 11:48:31 -0400

    bonus deliverables, plus a little marquee surprise

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,4 +1,11 @@
 class Episode < ApplicationRecord
     has_many :appearances, dependent: :destroy
     has_many :guests, through: :appearances
+
+
+    def average_app_rating
+       ratings = self.appearances.map {|app| app.rating}
+
+       ratings.sum(0.0) / ratings.count
+    end
 end
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -2,4 +2,13 @@ class Guest < ApplicationRecord
     has_many :appearances, dependent: :destroy
     has_many :episodes, through: :appearances
 
+    def sort_appearances
+        self.appearances.sort_by{|app| app.rating}.reverse!
+    end
+
+    def avg_rating
+       ratings = self.appearances.map {|app| app.rating}
+
+       ratings.sum(0.0) / ratings.count
+    end
 end
