d3a6ce4f57756e773f6f146235c78c1578cf86f9
kylejb <47502769+kylejb@users.noreply.github.com>
2020-07-24 11:28:41 -0400

    Advanced: MVP infastructure set

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -3,4 +3,7 @@ class Appearance < ApplicationRecord
   belongs_to :episode
 
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+
+  # Question 6 (Advanced) 
+  validates_uniqueness_of :guest_id, :scope => :episode_id
 end
