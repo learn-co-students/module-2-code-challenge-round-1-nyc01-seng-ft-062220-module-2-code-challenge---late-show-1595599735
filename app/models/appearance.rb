12eb0bc308085912dc092a55be363aba6eddc689
Aiannn <soktoevayan@gmail.com>
2020-07-24 11:51:51 -0400

    deliverables_2

--- app/models/appearance.rb
+++ app/models/appearance.rb
@@ -2,5 +2,6 @@ class Appearance < ApplicationRecord
     belongs_to :guest 
     belongs_to :episode 
 
-    validates :rating, numericality: {greater_than: 0}, numericality: {less_than: 6}
+    validates :rating, numericality: {greater_than: 0}
+    validates :rating, numericality: {less_than: 6}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -27,6 +27,6 @@ date = Date.parse('2015-09-08')
 end
 
 100.times do
-  Appearance.create(guest_id: Guest.all.sample.id, episode_id: Episode.all.sample.id, rating: rand(30) )
+  Appearance.create(guest_id: Guest.all.sample.id, episode_id: Episode.all.sample.id, rating: rand(5) )
 end
 
